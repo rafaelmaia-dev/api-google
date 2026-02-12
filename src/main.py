8c3c8f9f8eabac870125ebc9f3a9c1401195464e
Rafael Neto Maia <rafaelnetomaiar@gmail.com>
2026-02-11 22:41:24 -0300

    Format code with Black

--- src/main.py
+++ src/main.py
@@ -1,27 +1,33 @@
 import requests
-import os 
+import os
 
-url = "https://www.google.com.br" # Adicionando um link do Google a uma variável 
+url = "https://www.google.com.br"  # Adicionando um link do Google a uma variável
 
 try:
-    response = requests.get(url) # Aqui a variável 'response' recebe uma requição com o método 'get' do HTTP para buscar dados da URL
+    response = requests.get(
+        url
+    )  # Aqui a variável 'response' recebe uma requição com o método 'get' do HTTP para buscar dados da URL
 
-    if response.status_code == 200: # Aqui é uma condição verificando se a resposta da requição pode alcançar o resultado de sucesso
-        print('Sucesso! Status:', response.status_code)
-        print('Conteúdo (primeiros 100 chars):', response.text[:100])
+    if (
+        response.status_code == 200
+    ):  # Aqui é uma condição verificando se a resposta da requição pode alcançar o resultado de sucesso
+        print("Sucesso! Status:", response.status_code)
+        print("Conteúdo (primeiros 100 chars):", response.text[:100])
 
-        os.makedirs('files', exist_ok = True) # Criando o diretório sem erro se já existir
+        os.makedirs(
+            "files", exist_ok=True
+        )  # Criando o diretório sem erro se já existir
 
-        with open("files/page.html", "w", encoding = "UTF-8") as page:
-                page.write(response.text)
+        with open("files/page.html", "w", encoding="UTF-8") as page:
+            page.write(response.text)
 
         print("Página salva em files/page.html")
     else:
-        print(f'Status Inválido: {response.status_code}')
+        print(f"Status Inválido: {response.status_code}")
 
-except (requests.exceptions.RequestException, IOError, Exception) as e: # Aqui como o except válida uma condição como booleana, usei as 'tuplas' para adicionar os possíveis erros 
-    print(f'Erro no request: {e}.')
-
-
-
-
+except (
+    requests.exceptions.RequestException,
+    IOError,
+    Exception,
+) as e:  # Aqui como o except válida uma condição como booleana, usei as 'tuplas' para adicionar os possíveis erros
+    print(f"Erro no request: {e}.")
--- tests/test_main.py
+++ tests/test_main.py
@@ -3,20 +3,19 @@ import subprocess
 import os
 import sys
 
+
 class TestScriptExecution(unittest.TestCase):
 
     def test_script_creates_html_file(self):
         script_path = os.path.join("src", "main.py")
 
-        subprocess.run(
-            [sys.executable, script_path],
-            check=True
-        )
+        subprocess.run([sys.executable, script_path], check=True)
 
         self.assertTrue(
             os.path.exists("files/page.html"),
-            "O arquivo files/page.html não foi criado"
+            "O arquivo files/page.html não foi criado",
         )
+
 
 if __name__ == "__main__":
     unittest.main()
